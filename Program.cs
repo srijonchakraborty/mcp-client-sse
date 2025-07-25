c9a5aa0b6197ee6f4c74f0405d09a2565d4a7370
srijon202 <srijonchak@live.com>
2025-07-25 15:07:47 +0600

    ollama implementation basic added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using Azure.AI.OpenAI;
 using Microsoft.Extensions.AI;
 using OpenAI;
+using System.ClientModel;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,20 +13,41 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddOpenApi();
+
+//// Create an IChatClient using Azure OpenAI.
+//IChatClient client =
+//    new ChatClientBuilder(
+//        new AzureOpenAIClient(new Uri("<your-azure-openai-endpoint>"),
+//        new ApiKeyCredential(""))
+//        .GetChatClient("gpt-4o").AsIChatClient())
+//    .UseFunctionInvocation()
+//    .Build();
+
+//OPEN AI Implemtation 
 builder.Services.AddChatClient(services =>
     new ChatClientBuilder(
-        new OpenAIClient("OPENAI KEY")
+         new OpenAIClient("OPENAI KEY")
         .GetChatClient("gpt-4.1").AsIChatClient()
     )
     .UseFunctionInvocation()
     .Build());
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//Ollama
+//Need to update ollama implementation to use the new ChatClientBuilder
+//builder.Services.AddChatClient(services =>
+//    new ChatClientBuilder(
+//      new OllamaChatClient(new Uri("http://localhost:11434"), "llama3")
+//    )
+//    .UseFunctionInvocation()
+//    .Build());
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
 
 app.UseHttpsRedirection();
 
